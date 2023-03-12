0268f540048cf272ad5e249a606c5efe999eff51
indigosaurus <indigosaurus@hotmail.co.uk>
2023-03-12 23:23:01 +0000

    .

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './index.css';
 
 function App() {
+  const subject = "React";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to Abi's App!</p>
+        <p>Welcome to Abi's App, {subject}!</p>
       </header>
     </div>
   );
